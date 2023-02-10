ff6736d6bfb1c4367ef18dc6e1db4a5358c29d1a
mohamedA122ouz <studyatmohamed@gmail.com>
2023-02-10 14:22:55 +0200

    0.2.0

--- GPA/script/script.js
+++ GPA/script/script.js
@@ -2,7 +2,45 @@
 var allresponse = { index: 1 };
 var mainArea = document.querySelector('.content');
 var mainArea1 = document.querySelector('.content').innerHTML;
+const defaultValues = [4, 3.67, 3.33, 3, 2.67, 2.33, 2];
+let [A, Am, Bp, B, Cp, C, D] = [];
+if(!localStorage.gValues)
+[A, Am, Bp, B, Bm, Cp, C, D] = defaultValues
+function restoreValues(){
+    if(localStorage.getItem("lastGPA")){
+        document.querySelector('.ans').textContent = localStorage.getItem("lastGPA");
+    }
+    let gradesFiled = document.querySelectorAll(".grade");
+    if(localStorage.getItem("gValues")){
+        let oldValues = localStorage.getItem("gValues").split(',');
+        gradesFiled.forEach(
+            (el,i)=>{
+                el.value = parseFloat(oldValues[i]);
+            }
+        );
+        let oldV = localStorage.getItem("gValues").split(',').map(el=>{
+            return parseFloat(el);
+        });
+        [A, Am, Bp, B, Cp, C, D] = oldV;
+    }
+    else{
+        gradesFiled.forEach(
+            (el,i)=>{
+                el.value = defaultValues[i];
+            }
+        );
+    }
+};
+function clearOldValuesAndRestoreDefault(){
+    localStorage.removeItem("gValues");
+    resetDefaultValues();
+    restoreValues();
+    calc();
+}
 // console.log(mainArea);
+function resetDefaultValues(){
+    [A, Am, Bp, B, Bm, Cp, C, D] = defaultValues;
+}
 // get cum GPA info and edite it
 function getcum() {
     ajax("html/cum.html");
@@ -13,10 +51,28 @@ function replace() {
     allresponse.res1.toString();
     allresponse.res1 = allresponse.res1.replace(RegExp('{{num}}', 'g'), '1');
     mainArea.innerHTML = allresponse.res1;
+    restoreValues();
+}
+//set grades
+function setGrades(){
+    let newValues = [];
+    let gradesFiled = document.querySelectorAll(".grade");
+    gradesFiled.forEach(
+        (el,i)=>{
+            if(!isNaN(parseInt(el.value)))
+            newValues.push(parseFloat(el.value));
+            else{
+                newValues.push(defaultValues[i]);
+            }
+        }
+    );
+    localStorage.setItem("gValues",newValues);
+    [A, Am, Bp, B, Bm, Cp, C, D]  = newValues;
+    calc();
 }
 function addatcum() {
     allresponse.index++;
-    var newtable = '<div class="row col-md-5 col-sm-12 col-xs-12"><table><tr><th>GPA #' + allresponse.index + '</th><th>total Hours For GPA #' + allresponse.index + '</th></tr><tr><td><input type="text" placeholder="0" pattern="^[0-9]$"id="g' + allresponse.index + '"></td><td><input type="text" placeholder="0" id="h' + allresponse.index + '"></td></tr></table></div>';
+    var newtable = '<div class="row col-md-5 col-sm-12 col-xs-12"><table><tr><th>GPA #' + allresponse.index + '</th><th>total Hours For GPA #' + allresponse.index + '</th></tr><tr><td><input type="text" placeholder="0" id="g' + allresponse.index + '"></td><td><input type="text" placeholder="0" id="h' + allresponse.index + '"></td></tr></table></div>';
     newtable.replace(RegExp('{{num}}', 'g'), allresponse.index.toString());
     var hours = [];
     var gpa = [];
@@ -30,6 +86,17 @@ function addatcum() {
         document.querySelector('#h' + (i + 1)).value = hours[i];
         document.querySelector('#g' + (i + 1)).value = gpa[i];
     }
+    scrollToBottom();
+}
+function ifString(value) {
+    if (isNaN(parseInt(value))) {
+        // A,Am,Bp,B,Bm,Cp,C,D
+        value = value.toUpperCase();
+        return value === "A" ? A : value === "A-" ? Am : value === "B+" ? Bp : value === "B" ? B : value === "C+" ? Cp : value === "C" ? C : value === "D" ? D : 0;
+    }
+    else {
+        return value;
+    }
 }
 function calc() {
     var hours = [];
@@ -38,7 +105,7 @@ function calc() {
     var top = 0;
     for (var i = 0; i < allresponse.index; i++) {
         hours[i] = document.querySelector('#h' + (i + 1)).value;
-        gpa[i] = document.querySelector('#g' + (i + 1)).value;
+        gpa[i] = ifString(document.querySelector('#g' + (i + 1)).value);
         if (hours[i] == "")
             hours[i] = 0;
         tHr += parseInt(hours[i]);
@@ -48,28 +115,7 @@ function calc() {
         tHr = 1;
     // console.log(top / tHr);
     document.querySelector('.ans').textContent = (top / tHr);
-
-}
-// get cur GPA info and edite//////////////////////////////////////////
-function getcur() {
-    ajax("html/cur.html");
-}
-function addatcur() {
-    allresponse.index++;
-    var newrow = '<tr><td><input type="text" placeholder="0" pattern="^[0-9]$"id="g' + allresponse.index + '"></td><td><input type="text" placeholder="0" id="h' + allresponse.index + '"></td></tr>';
-    newrow.replace(RegExp('{{num}}', 'g'), allresponse.index.toString());
-    var hours = [];
-    var gpa = [];
-    for (var i = 0; i < (allresponse.index - 1); i++) {
-        hours[i] = document.querySelector('#h' + (i + 1)).value;
-        gpa[i] = document.querySelector('#g' + (i + 1)).value;
-    }
-    var table = document.querySelector('table');
-    table.innerHTML += newrow;
-    for (var i = 0; i < (allresponse.index - 1); i++) {
-        document.querySelector('#h' + (i + 1)).value = hours[i];
-        document.querySelector('#g' + (i + 1)).value = gpa[i];
-    }
+    localStorage.setItem("lastGPA",(top/tHr));
 }
 ///back home///////////////////////////////////////////////////////////
 function home() {
@@ -90,4 +136,11 @@ function ajax(place) {
             replace();
         }
     }
+}
+////scrolltoBottom
+function scrollToBottom(){
+    let rootEl = document.querySelector(".root");
+    rootEl.style.scrollBehavior = "smooth";
+    rootEl.scrollTop = rootEl.scrollHeight;
+    
 }
