ffe085437821583b77d6b15f9e52a2eefb33180f
prime11 <studyatmohamed@gmail.com>
2024-06-11 13:07:23 +0300

    make list channel videos is possible

--- Youtube/js/script.js
+++ Youtube/js/script.js
@@ -564,8 +564,8 @@ function hideul() {
     atGlobal.showul = true;
 }
 function openChannel(url) {
-    // getdata("https://youtube-6rrj.onrender.com/search?q="+url,"json","mainroute");
-    getdata(`http://localhost:3002/channel?q=${url}`, "json", "mainroute");
+    getdata("https://youtube-6rrj.onrender.com/channel?q="+url,"json","mainroute");
+    // getdata(`http://localhost:3002/channel?q=${url}`, "json", "mainroute");
 }
 ///////////////////////////////////////////MAIN FUNCTION//////////////////////////////////////////////////
 var main = (function (event) {
@@ -667,10 +667,10 @@ var main = (function (event) {
                 var youtubeAPI6 = "http://192.168.1.5:3002/search?q=" + processedSearch;
                 var youtubeAPI3 = "https://youtube-6rrj.onrender.com/search?q=" + processedSearch;
                 var tryy = "data/data2.json";
-                getdata(youtubeAPI2, "json", "mainroute");
+                // getdata(youtubeAPI2, "json", "mainroute");
                 //getdata(youtubeAPI6, "json", "mainroute");
                 youtubeDt = {};
-                // getdata(youtubeAPI3, "json", "mainroute");
+                getdata(youtubeAPI3, "json", "mainroute");
                 // getdata(youtubeAPI, "json", "mainroute");
                 // getdata(tryy, "json", "mainroute");
                 // console.log(link);
